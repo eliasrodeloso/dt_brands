0034d182280856c0685d8af2638490d87dbaf4f7
Elias Rafael Rodelo <eliasrodeloso@gmail.com>
2017-04-22 10:24:30 -0500

    Presenting elements on FO correctly: First Try

--- dt_brands.php
+++ dt_brands.php
@@ -231,24 +231,19 @@ class Dt_brands extends Module implements WidgetInterface
 
     public function getWidgetVariables($hookName = null, array $configuration = [])
     {
-        $address = $this->context->shop->getAddress();
-        $contact_infos = [
-            'company' => Configuration::get('PS_SHOP_NAME'),
-            'address' => [
-                'formatted' => AddressFormat::generateAddress($address, array(), '<br />'),
-                'address1' => $address->address1,
-                'address2' => $address->address2,
-                'postcode' => $address->postcode,
-                'city' => $address->city,
-                'state' => (new State($address->id_state))->name[$this->context->language->id],
-                'country' => (new Country($address->id_country))->name[$this->context->language->id],
-            ],
-            'phone' => Configuration::get('PS_SHOP_PHONE'),
-            'fax' => Configuration::get('PS_SHOP_FAX'),
-            'email' => Configuration::get('PS_SHOP_EMAIL'),
-        ];
-        return [
-            'contact_infos' => $contact_infos,
-        ];
+        $brands = array();
+        for ($i = 0; $i <= 10; $i++)
+        {
+            $brand = array(
+                'name' => $i,
+                'desc' => $i + $i,
+            );
+            $brands[$i] = $brand;
+        }
+        
+        
+        return array(
+            'brands' => $brands
+        );
     }
 }
--- dt_brands.tpl
+++ dt_brands.tpl
@@ -1,4 +1,20 @@
 <div class="brands">
+
+	<ul>
+		
+		{foreach from=$brands item=brand}
+			<ul>	
+				<h1>{$brand.name}</h1>
+				<p>
+					{$brand.desc}
+				</p>
+			</ul>
+		{/foreach}
+		
+	</ul>
+
+	{*{$brands.name}*}
+
 	<p>
 		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut neque labore excepturi sit animi quod eum numquam, sequi, id natus fugiat perspiciatis molestias ab iste quasi, quam, est consequuntur accusantium.
 	</p>
